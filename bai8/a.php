d6d138b9331ad3b285946a630ec112e9d6b3c3d8
thienth32 <64114903+thienth32@users.noreply.github.com>
2021-11-30 10:44:12 +0700

    comment login đăng nhập

--- bai8/a.php
+++ bai8/a.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+// nếu không tồn tại phần tử 'auth' của session hoặc giá trị của phần tử này = null
+// thì điều hướng website về trang login
 if(!isset($_SESSION['auth']) || $_SESSION['auth'] == null){
     header('location: login.php');die;
 }
--- bai8/post_login.php
+++ bai8/post_login.php
@@ -1,16 +1,22 @@
 <?php
 
-use function PHPSTORM_META\type;
-
 session_start();
+// nhận dữ liệu từ form
 $email = $_POST['email'];
 $password = $_POST['password'];
+
+// tạo kết nối đến csdl
 $connect = new PDO("mysql:host=127.0.0.1;dbname=php1_lab4;charset=utf8", "root", "12345678");
+// chuẩn bị sẵn 1 câu lệnh sql để lấy user dựa vào email nhận đc từ form
 $getUserByEmail = "select * from users where email = '$email'";
 $stmt = $connect->prepare($getUserByEmail);
+// thực thi câu lệnh sql với csdl
 $stmt->execute();
+// nhận dữ liệu trả về từ csdl
 $user = $stmt->fetch();
 
+// nếu có user và mk trong csdl khớp với mk người dùng điền vào => tạo phần tử auth của biến
+// session và điều hướng website về trang a.php
 if($user && password_verify($password, $user['password'])){
     $_SESSION['auth'] = [
         'id' => $user['id'],
@@ -21,7 +27,7 @@ if($user && password_verify($password, $user['password'])){
     header('location: a.php');
     die;
 }
-
+// không thỏa mãn thì điều hướng ngược về trang login với 1 message
 header("location: login.php?msg=Đăng nhập không thành công!");
 
 ?>
